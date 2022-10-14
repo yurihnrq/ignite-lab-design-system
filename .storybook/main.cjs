4058b30331d16c90e6dd742206ae92069d5065d4
Yuri Henrique B. Maciel <yurihenrique.bernardes@gmail.com>
2022-10-13 23:52:57 -0300

    ci: storybook publish workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,4 +16,9 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite-lab-design-system/";
+    }
+  },
 };
